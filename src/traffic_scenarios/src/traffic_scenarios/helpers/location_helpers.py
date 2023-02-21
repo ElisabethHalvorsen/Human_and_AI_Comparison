af54fe080a978514d41315471cffa2a9ff70306d
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2023-02-21 21:29:57 +0000

    refactor: make you choose the tolerance instead of fixed of 10

--- src/traffic_scenarios/src/traffic_scenarios/helpers/location_helpers.py
+++ src/traffic_scenarios/src/traffic_scenarios/helpers/location_helpers.py
@@ -2,11 +2,9 @@
 from carla import Location, Transform, Rotation
 from traffic_scenarios.utils.connect import Connect
 
-TOLERANCE = 10
 
-
-def is_at_end(loc: Location, end: Location) -> bool:
-    return (end.x - TOLERANCE < loc.x < end.x + TOLERANCE) and (end.y - TOLERANCE < loc.y < end.y + TOLERANCE)
+def is_at_end(loc: Location, end: Location, tolerance=10) -> bool:
+    return (end.x - tolerance < loc.x < end.x + tolerance) and (end.y - tolerance < loc.y < end.y + tolerance)
 
 
 def get_current_location_carla(connect: Connect):
