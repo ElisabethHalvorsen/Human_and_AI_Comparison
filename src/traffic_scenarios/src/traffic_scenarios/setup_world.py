d41ae187a29aeab11e9fe8c76878c4bc54ad587b
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2023-03-09 09:35:51 +0000

    fix: change viewing point to see entire scenario

--- src/traffic_scenarios/src/traffic_scenarios/setup_world.py
+++ src/traffic_scenarios/src/traffic_scenarios/setup_world.py
@@ -5,9 +5,10 @@ from carla import Transform, Location, Rotation
 from scenario_generation.scenarios_generation import InitialiseScenario
 
 VIEWING_POINT = Transform(
-    Location(x=99.611725, y=-78.628754, z=54.584332),
-    Rotation(pitch=-86.137238, yaw=-178.290970, roll=0.000341)
+    Location(x=102.215004, y=-78.551079, z=93.157410),
+    Rotation(pitch=-86.137344, yaw=-178.290955, roll=0.000343)
 )
+
 
 if __name__ == '__main__':
     connect = Connect()
