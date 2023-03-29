3e596c1cb70a2ce9ce1fcdfbd9236533642f0513
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2023-03-29 15:22:03 +0100

    fix: when below 10, use 1 as standard min_samples

--- scenario_generation/src/scenario_generation/diversity_evaluation.py
+++ scenario_generation/src/scenario_generation/diversity_evaluation.py
@@ -13,19 +13,23 @@ SCENARIO_PATH = os.path.join(rospkg.RosPack().get_path('scenario_generation'), '
                              'scenarios', f'scenarios_{Weather(0)}.csv')
 
 population = pd.read_csv(SCENARIO_PATH)
-print("Population shape:", int(population.shape[0]/100))
-dbscan = DBSCAN(eps=5, min_samples=int(population.shape[0]/100))
-dbscan.fit_predict(population)
-pred = dbscan.fit_predict(population)
-labels = dbscan.labels_
+min_samples = population.shape[0]/100.0
+if min_samples < 1:
+    min_samples = 1
+else:
+    min_samples = int(min_samples)
+print("Population shape:", min_samples)
+dbscan = DBSCAN(eps=5, min_samples=min_samples)
+labels = dbscan.fit_predict(population)
 
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
 n_noise_ = list(labels).count(-1)
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
 
 print("Estimated number of clusters: %d" % n_clusters_)
 print("Estimated number of noise points: %d" % n_noise_)
 
-labels = dbscan.labels_
 unique_labels = np.unique(labels)
 # unique_labels = unique_labels[unique_labels != -1]  # remove noise points, as we don't want to count them
 cluster_sizes = []
@@ -45,9 +49,3 @@ print("Diversity:", diversity)
 
 # plot
 plot_3d(population, labels, title='DBSCAN')
-
-# Therefore, a low entropy value suggests that the data is concentrated in a few clusters, while a high entropy value
-# suggests that the data is more evenly spread across the clusters. In the context of measuring diversity,
-# a high entropy value indicates that the dataset is more diverse, as there are many clusters with a roughly equal
-# number of data points. On the other hand, a low entropy value indicates that the dataset is less diverse,
-# as most of the data is concentrated in one or a few clusters.
