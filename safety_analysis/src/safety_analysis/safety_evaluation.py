e1a1411b5caaa52adfb5102349f43bdb8e119a4c
ElisabethHalvorsen <elisabeth1999@sf-nett.no>
2023-04-03 19:59:09 +0100

    style: return all nessecary variables

--- safety_analysis/src/safety_analysis/safety_evaluation.py
+++ safety_analysis/src/safety_analysis/safety_evaluation.py
@@ -2,6 +2,8 @@
 import os
 import rospkg
 import pandas as pd
+import rospy
+from carla import Vector3D
 
 DATA_PATH = os.path.join(rospkg.RosPack().get_path('safety_analysis'), 'src', 'safety_analysis', 'data')
 
@@ -11,21 +13,24 @@ def get_last_file():
     files.sort(key=lambda x: os.path.getmtime(os.path.join(DATA_PATH, x)))
     return files[-1]
 
-
 def score_file(file: str):
     df = pd.read_csv(os.path.join(DATA_PATH, file))
     rows = df.shape[0]
+    total_behaviours = rows
+    dangerous_behaviours = df['Dangerous'].sum()
     if df['Collided'].any():
         score = -1
-        print("Collision detected")
+        rospy.logwarn("Collision detected")
     else:
-        print("Total behaviours: ", rows)
-        print("Dangerous behaviours: ", df['Dangerous'].sum())
         score = (rows - df['Dangerous'].sum()) / rows
-    return score
+    return score, total_behaviours, dangerous_behaviours
 
 
 if __name__ == "__main__":
     print("Last file: ", get_last_file())
-    score = score_file(get_last_file())
+    score, total_behaviours, dangerous_behaviours = score_file(get_last_file())
     print("score: ", score)
+    print("Total behaviours: ", total_behaviours)
+    print("Dangerous behaviours: ", dangerous_behaviours)
+
+
